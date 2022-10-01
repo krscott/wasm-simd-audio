33ab87df9eaf15890ba273f2fb1a0558cc93dd26
Kris Scott <kscott91@gmail.com>
2022-10-01 09:44:20 -0500

    Add prettier-plugin-tailwindcss

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,7 @@
-import "./app.css";
-
 export function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">Hello world!</h1>
     </div>
   );
 }
