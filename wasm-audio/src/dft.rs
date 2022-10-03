36a9f20c9fed197ff0b916b2bd7ccc679aae8a19
Kris Scott <kscott91@gmail.com>
2022-10-03 18:09:51 -0500

    Add idft

--- wasm-audio/src/dft.rs
+++ wasm-audio/src/dft.rs
@@ -2,11 +2,20 @@ use core::f32::consts::PI;
 use rustfft::num_complex::Complex;
 
 pub fn dft(input: &[Complex<f32>], output: &mut [Complex<f32>]) {
+    dft_helper(input, output, false);
+}
+
+#[allow(dead_code)]
+pub fn idft(input: &[Complex<f32>], output: &mut [Complex<f32>]) {
+    dft_helper(input, output, true);
+}
+
+fn dft_helper(input: &[Complex<f32>], output: &mut [Complex<f32>], inverse: bool) {
     assert_eq!(input.len(), output.len());
 
     let n = input.len();
 
-    let wn = 2.0 * PI / n as f32;
+    let wn = 2.0 * PI / n as f32 * if inverse { -1. } else { 1. };
 
     (0..n).into_iter().for_each(|i| {
         let mut y = &mut output[i];
@@ -22,6 +31,11 @@ pub fn dft(input: &[Complex<f32>], output: &mut [Complex<f32>]) {
 
             y.re += x.re * c + x.im * s;
             y.im += -x.re * s + x.im * c;
-        })
+        });
+
+        if inverse {
+            y.re /= n as f32;
+            y.im /= n as f32;
+        }
     })
 }
