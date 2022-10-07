76f824dbddc08d2cc2702f6729183240de7077d5
Kris Scott <kscott91@gmail.com>
2022-10-06 23:04:11 -0500

    Add comments for clarity

--- wasm-audio/src/simd_cooley_tukey2.rs
+++ wasm-audio/src/simd_cooley_tukey2.rs
@@ -45,24 +45,36 @@ pub fn simd_cooley_tukey_fft2(input: &[Complex<f32>], output: &mut [Complex<f32>
     })
 }
 
+/// Calculate `left0 * right0` and `left1 * right1` in parallel using SIMD.
 #[target_feature(enable = "simd128")]
 fn simd_complex_mul(
-    op0_a: Complex<f32>,
-    op0_b: Complex<f32>,
-    op1_a: Complex<f32>,
-    op1_b: Complex<f32>,
+    left0: Complex<f32>,
+    right0: Complex<f32>,
+    left1: Complex<f32>,
+    right1: Complex<f32>,
 ) -> (Complex<f32>, Complex<f32>) {
-    let a_b = f32x4(op0_a.re, op0_a.im, op1_a.re, op1_a.im);
-    let c_c = f32x4(op0_b.re, op0_b.re, op1_b.re, op1_b.re);
-    let d_d = f32x4(op0_b.im, op0_b.im, op1_b.im, op1_b.im);
+    // In all calculations, both pairs will do the same operations.
+    // So, we only need to reason about the first 2 lanes as if they were f32x2:
+    //   (a + ib)*(c + id) = (ac - bd) + i(bc + ad)
 
-    // Second parameter is not used
+    // a | b
+    let a_b = f32x4(left0.re, left0.im, left1.re, left1.im);
+    // c | c
+    let c_c = f32x4(right0.re, right0.re, right1.re, right1.re);
+    // d | d
+    let d_d = f32x4(right0.im, right0.im, right1.im, right1.im);
+
+    // b | a - Get by swapping lanes of a_b (second parameter is not used)
     let b_a = u32x4_shuffle::<1, 0, 3, 2>(a_b, a_b);
 
+    // ac | bc
     let ac_bc = f32x4_mul(a_b, c_c);
+    // bd | ad
     let bd_ad = f32x4_mul(b_a, d_d);
 
+    // ac-bd |        - Real output
     let acmbd_bcmad = f32x4_sub(ac_bc, bd_ad);
+    //       | bc+ad  - Imaginary output
     let acpbd_bcpad = f32x4_add(ac_bc, bd_ad);
 
     (
